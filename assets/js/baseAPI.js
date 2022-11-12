4190e1bd7525f92d5940be42872bd2a55ceb8e1d
HH <1653024949@qq.com>
2022-11-12 14:02:28 +0800

    init project

---
+++ assets/js/baseAPI.js
@@ -0,0 +1,8 @@
+$.ajaxPrefilter(function(options){
+    console.log(options.url)
+    options.url='http://www.liulongbin.top:3007'+options.url
+    
+    if(options.url.indexOf('/my/')!==-1){
+        options.headers={Authorization:localStorage.getItem('token')||''}
+    }
+})
